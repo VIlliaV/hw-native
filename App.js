de8d10152347fc5204d5c4d2c5727c8bcf012177
Illia <illia.vasiliev@gmail.com>
2024-03-03 23:32:28 +0200

    ADD || styleSheet

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  ImageBackground,
+  Platform,
+  StyleSheet,
+} from "react-native";
+
+import { useFonts } from "expo-font";
 import { styles } from "./style/styles";
 
+import back_ground from "./assets/image/Photo BG.webp";
+import back_ground_2x from "./assets/image/Photo BGx2.webp";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Medium": require("./assets/fonts/Inter-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>loading</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello Ketty</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={Platform.OS === "ios" ? back_ground_2x : back_ground}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Hello KeT</Text>
+        <Text style={{ fontFamily: "Roboto-Medium" }}>Hello KeT</Text>
+        <Text style={{ fontFamily: "Roboto-Bold" }}>Hello </Text>
+        <Text style={{ fontFamily: "Inter-Medium" }}>Hello KeT</Text>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   image: {
+//     flex: 1,
+//     justifyContent: "center",
+//   },
+//   text: {
+//     fontFamily: "Roboto-Regular",
+//     ...Platform.select({
+//       ios: {
+//         fontSize: 30,
+//       },
+//       android: {
+//         color: "#ffffff",
+//       },
+//     }),
+//   },
+// });
--- style/styles.js
+++ style/styles.js
@@ -1,10 +1,22 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
+  },
+  image: {
+    flex: 1,
     justifyContent: "center",
+  },
+  text: {
+    fontFamily: "Roboto-Medium",
+    ...Platform.select({
+      ios: {
+        fontSize: 30,
+      },
+      android: {
+        color: "#ffffff",
+      },
+    }),
   },
 });
