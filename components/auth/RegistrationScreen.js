425df5a95fa0b386d9bbb4db4c4338a908b9f0bc
Illia <illia.vasiliev@gmail.com>
2024-03-13 00:29:59 +0200

    UPD | registrationScreen

--- components/auth/RegistrationScreen.js
+++ components/auth/RegistrationScreen.js
@@ -1,48 +1,11 @@
-import {
-  KeyboardAvoidingView,
-  Platform,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "../../style/styles";
 import { useState } from "react";
+import KeyboardAvoidingViewComp from "../KeyboardAvoidingViewComp/KeyboardAvoidingViewComp";
+import AuthComp from "./AuthComp";
 
 const RegistrationScreen = () => {
-  const [text, setText] = useState("");
-  return (
-    <KeyboardAvoidingView // визначаємо ОС та налаштовуємо поведінку клавіатури
-      behavior={Platform.OS === "ios" ? "padding" : "padding"}
-      style={styles.popUp}
-    >
-      <Text style={styles.title}>Реєстрація</Text>
-      <Text style={styles.text}>Hello KeT</Text>
-      <Text style={{ fontFamily: "Roboto-Bold" }}>Hello </Text>
-      <Text style={{ fontFamily: "Inter-Medium" }}>Hello KeT</Text>
-      <Text style={{ fontFamily: "Inter-Medium" }}>Hello KeT</Text>
-      <Text style={{ fontFamily: "Inter-Medium" }}>Hello KeT</Text>
-      <Text style={{ fontFamily: "Inter-Medium" }}>Hello KeT</Text>
-
-      <TextInput
-        placeholder="Type text HERE"
-        value={text}
-        onChangeText={setText}
-        style={styles.textInput}
-      />
-      <TextInput
-        placeholder="Type text HERE"
-        // value={text}
-        // onChangeText={setText}
-        style={styles.textInput}
-      />
-    </KeyboardAvoidingView>
-
-    // <View style={styles.popUp}>
-
-    //   <Text style={{ fontFamily: "Inter-Medium" }}>Hello KeT</Text>
-    //   <Text style={{ fontFamily: "Inter-Medium" }}>TEST</Text>
-    // </View>
-  );
+  return <AuthComp auth="register" />;
 };
 
 export default RegistrationScreen;
