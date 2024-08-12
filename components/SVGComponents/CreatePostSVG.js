ec2c0a327fc90089a9a096487d96775b7b638407
Illia <illia.vasiliev@gmail.com>
2024-08-13 00:16:37 +0300

    ADD || svg components for bottom-tabs

---
+++ components/SVGComponents/CreatePostSVG.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Svg, { Rect, Path } from "react-native-svg";
+import { color } from "../../style/color";
+
+const CreatePostSVG = (props) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill={props.fill}
+  >
+    <Rect width="24" height="24" fill={props.fill} />
+    <Path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12.3 8.1H11.7V11.7H8.1V12.3H11.7V15.9H12.3V12.3H15.9V11.7H12.3V8.1Z"
+      stroke={props.fill === color.accent ? color.bg : color.primary}
+      fill-opacity="0.8"
+    />
+  </Svg>
+);
+
+export default CreatePostSVG;
---
+++ components/SVGComponents/PostsSVG.js
@@ -0,0 +1,53 @@
+import * as React from "react";
+import Svg, { Rect, Path } from "react-native-svg";
+import { color } from "../../style/color";
+
+const PostsSVG = (props) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/Svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <Rect width="24" height="24" fill={props.fill} />
+    <Path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M3 3H10V10H3V3Z"
+      stroke={props.fill === color.accent ? color.bg : color.primary}
+      stroke-opacity="0.8"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <Path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M14 3H21V10H14V3Z"
+      stroke={props.fill === color.accent ? color.bg : color.primary}
+      stroke-opacity="0.8"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <Path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M14 14H21V21H14V14Z"
+      stroke={props.fill === color.accent ? color.bg : color.primary}
+      stroke-opacity="0.8"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <Path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M3 14H10V21H3V14Z"
+      stroke={props.fill === color.accent ? color.bg : color.primary}
+      stroke-opacity="0.8"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </Svg>
+);
+
+export default PostsSVG;
