324e4b07e2f1d916b14cd075f34b3d61e80ee4bf
Illia <illia.vasiliev@gmail.com>
2024-08-21 00:45:27 +0300

    ADD || PlugCamera

--- screens/nestedScreens/CommentsScreen.js
+++ screens/nestedScreens/CommentsScreen.js
@@ -1,5 +1,11 @@
+import { Text, View } from "react-native";
+
 const CommentsScreen = () => {
-  return <></>;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>CommentsScreen!</Text>
+    </View>
+  );
 };
 
 export default CommentsScreen;
--- screens/nestedScreens/MapScreen.js
+++ screens/nestedScreens/MapScreen.js
@@ -1,5 +1,11 @@
+import { Text, View } from "react-native";
+
 const MapScreen = () => {
-  return <></>;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>MapScreen!</Text>
+    </View>
+  );
 };
 
 export default MapScreen;
