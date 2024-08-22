666152799131cc20b12275e8163c46128c0197ed
Illia <illia.vasiliev@gmail.com>
2024-08-22 21:43:07 +0300

    FIX || HeadContainer

--- components/HeadContainer.js
+++ components/HeadContainer.js
@@ -1,23 +1,21 @@
-import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
+import { View } from "react-native";
 import { color } from "../style/color";
 
 const HeadContainer = ({ children }) => {
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View
-        style={{
-          flex: 1,
-          position: "relative",
-          backgroundColor: color.bg,
-          paddingHorizontal: 16,
-          paddingTop: 32,
-          paddingBottom: 84,
-          // minHeight: "100%",
-        }}
-      >
-        {children}
-      </View>
-    </TouchableWithoutFeedback>
+    <View
+      style={{
+        flex: 1,
+        position: "relative",
+        backgroundColor: color.bg,
+        paddingHorizontal: 16,
+        paddingTop: 32,
+        // paddingBottom: 84,
+        // minHeight: "100%",
+      }}
+    >
+      {children}
+    </View>
   );
 };
 
