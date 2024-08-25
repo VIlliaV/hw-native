83345e6d9b7b8e895e21bdaa67337fd354e006d9
Illia <illia.vasiliev@gmail.com>
2024-08-25 23:36:27 +0300

    UPD || Nested router // upd styling

--- routes/Nested.js
+++ routes/Nested.js
@@ -2,14 +2,26 @@ import React from "react";
 import CommentsScreen from "../screens/nestedScreens/CommentsScreen";
 import MapScreen from "../screens/nestedScreens/MapScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+import { styles } from "../style/styles";
+import BackButton from "../components/buttons/BackButton";
 
 const Nested = () => {
   const NestedStack = createStackNavigator();
   return (
-    <NestedStack.Navigator backBehavior="none">
+    <NestedStack.Navigator
+      backBehavior="none"
+      screenOptions={{
+        headerStyle: {
+          height: 88,
+        },
+        headerTitleStyle: styles.headerTitle,
+        headerTitleAlign: "center",
+      }}
+    >
       <NestedStack.Screen
         name="MapScreen"
         component={MapScreen}
+
         // options={{
         //   headerShown: false,
         // }}
@@ -19,6 +31,7 @@ const Nested = () => {
         component={CommentsScreen}
         options={{
           title: "Коментарі",
+          headerLeft: () => <BackButton />,
         }}
       />
     </NestedStack.Navigator>
--- screens/nestedScreens/CommentsScreen.js
+++ screens/nestedScreens/CommentsScreen.js
@@ -1,10 +1,206 @@
-import { Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import HeadContainer from "../../components/HeadContainer";
+import postPhoto from "../../assets/image/postPhoto.jpg";
+import { styles } from "../../style/styles";
+import { formatDate } from "../../utils/formatDate";
+import { color } from "../../style/color";
+import avatar from "../../assets/image/avatarImage.jpg";
+import { useEffect, useState } from "react";
+
+const jsonCommentsData = require("../../base/comments.json");
 
 const CommentsScreen = () => {
+  // const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+  // const [keyboardHeight, setKeyboardHeight] = useState(0);
+  // // console.log("🚀 ~ keyboardHeight:", keyboardHeight);
+
+  // useEffect(() => {
+  //   const keyboardDidShowListener = Keyboard.addListener(
+  //     "keyboardDidShow",
+  //     (e) => {
+  //       setKeyboardVisible(true);
+  //       setKeyboardHeight(e.endCoordinates.height);
+  //     }
+  //   );
+  //   const keyboardDidHideListener = Keyboard.addListener(
+  //     "keyboardDidHide",
+  //     () => {
+  //       setKeyboardVisible(false);
+  //       setKeyboardHeight(0);
+  //     }
+  //   );
+
+  //   return () => {
+  //     keyboardDidHideListener.remove();
+  //     keyboardDidShowListener.remove();
+  //   };
+  // }, []);
+
+  const [comment, setComment] = useState("");
+
+  const { comments } = jsonCommentsData;
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>CommentsScreen!</Text>
-    </View>
+    <HeadContainer>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "position" : "padding"}
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 70 : -140}
+      >
+        <FlatList
+          data={comments}
+          ListFooterComponent={
+            <View
+              style={{
+                height: 50,
+                marginBottom: 34,
+              }}
+            ></View>
+          }
+          ListHeaderComponent={
+            <Image
+              source={postPhoto}
+              resizeMode="cover"
+              style={{
+                ...styles.image,
+                borderRadius: 8,
+                width: "100%",
+                // height: 240,
+                marginBottom: 32,
+              }}
+            />
+          }
+          renderItem={({ item }) => (
+            <View
+              style={{
+                gap: 16,
+                display: "flex",
+                flexDirection:
+                  item.holderComment === "email@example.com"
+                    ? "row-reverse"
+                    : "row",
+                marginBottom: 24,
+              }}
+            >
+              <Image
+                source={avatar}
+                resizeMode="cover"
+                style={{
+                  borderRadius: 28,
+                  width: 28,
+                  height: 28,
+                }}
+              />
+
+              <View
+                style={{
+                  backgroundColor: " rgba(0, 0, 0, 0.03)",
+                  borderTopRightRadius: 6,
+                  borderBottomRightRadius: 6,
+                  borderBottomLeftRadius: 6,
+                  padding: 16,
+                  flex: 1,
+                }}
+              >
+                <Text
+                  style={{
+                    color: color.primary,
+                    fontFamily: "Roboto-Regular",
+                    fontSize: 13,
+                    lineHeight: 18,
+                    marginBottom: 8,
+                  }}
+                >
+                  {item.comment}
+                </Text>
+                <Text
+                  style={{
+                    color: color.placeholder,
+                    fontFamily: "Roboto-Regular",
+                    fontSize: 10,
+                    textAlign:
+                      item.holderComment === "email@example.com" ? "" : "right",
+                  }}
+                >
+                  {formatDate(item.dateComment)}
+                </Text>
+              </View>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+        {/* </View> */}
+
+        <View
+          style={{
+            width: "100%",
+            // styleAuth.passwordBox
+            borderWidth: 1,
+            position: "absolute",
+            bottom: 34,
+            // left: "50%",
+            // transform: [{ translateX: -35 }],
+
+            // height: 50,
+            // borderRadius: 20,
+            backgroundColor: "red",
+            // marginBottom: 34,
+            // paddingBottom: isKeyboardVisible ? 84 : 34,
+            // alignItems: "center",
+            zIndex: 100,
+          }}
+        >
+          <TextInput
+            // {...inputPasswordProps}
+            // secureTextEntry={isPasswordHide}
+            value={comment}
+            onChangeText={(value) => setComment(value)}
+            // onFocus={() => {
+            //   setInputOnFocus(inputPasswordProps.placeholder);
+            // }}
+            // onBlur={() => {
+            //   setInputOnFocus(null);
+            // }}
+            onSubmitEditing={Keyboard.dismiss}
+            style={{
+              // ...styleAuth.inputAuth({
+              //   inputOnFocus,
+              //   type: inputPasswordProps.placeholder,
+              // }),
+              ...styles.textInput,
+              marginBottom: 0,
+            }}
+          />
+          {/* <TouchableOpacity
+            style={
+              {
+                // styleAuth.showPassword
+              }
+            }
+            activeOpacity={0.6}
+          >
+            <Text
+              onPress={() => {}}
+              style={{ ...styles.text, color: color.secondary }}
+            >
+              текст
+            </Text>
+          </TouchableOpacity> */}
+        </View>
+      </KeyboardAvoidingView>
+    </HeadContainer>
   );
 };
 
