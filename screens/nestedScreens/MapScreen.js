d4ca85aca497987d1e14e436ac002012c5af321c
Illia <illia.vasiliev@gmail.com>
2024-09-01 20:21:57 +0300

    UPD || MapScreen

--- screens/nestedScreens/MapScreen.js
+++ screens/nestedScreens/MapScreen.js
@@ -2,55 +2,70 @@ import { Dimensions, StyleSheet, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
+// import avatarImage from "../../assets/image/avatarPNG.png";
+import { color } from "../../style/color";
 
 const MapScreen = () => {
   const [location, setLocation] = useState(null);
 
+  const name = "marker";
+  const description = "description";
+  const locationMarker = {
+    latitude: 49.81122943769388,
+    longitude: 24.024978248853097,
+  };
   useEffect(() => {
-    (async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        console.log("Permission to access location was denied");
-      }
-
+    const getLocation = async () => {
       const location = await Location.getCurrentPositionAsync({});
       const coords = {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
       };
+      console.log("🚀 ~ coords:", coords);
       setLocation(coords);
-    })();
+    };
+    getLocation();
   }, []);
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.mapStyle}
         // provider="google"
         region={{
-          ...location,
+          ...locationMarker,
           latitudeDelta: 0.005,
           longitudeDelta: 0.005,
         }}
         showsUserLocation={true}
         mapType="standard"
-        minZoomLevel={18}
+        // minZoomLevel={18}
         cameraZoomRange={{
           minCenterCoordinateDistance: 300,
           // maxCenterCoordinateDistance: 50000,
           animated: false,
         }}
-        onMapReady={() => console.log("Map is ready")}
-        onRegionChange={() => console.log("Region change")}
+        // onMapReady={() => console.log("Map is ready")}
+        // onRegionChange={() => console.log("Region change")}
         // loadingEnabled={true}
       >
         {location && (
-          <Marker title="I am here" coordinate={location} description="Hello" />
+          <Marker
+            title="ти тут"
+            coordinate={location}
+            // image={avatarImage}
+            pinColor={color.accent}
+          />
+        )}
+        {locationMarker && (
+          <Marker
+            title={name}
+            coordinate={locationMarker}
+            description={description}
+          />
         )}
       </MapView>
     </View>
-    // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    //   <Text>MapScreen!</Text>
-    // </View>
   );
 };
 
